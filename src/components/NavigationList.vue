717a61baf829f79be433c014253dfea8d55a0fe0
aaron <aaron.b@com>
2019-05-13 16:39:27 -0700

    left column and basic nav set up

---
+++ src/components/NavigationList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul id="navigationList">
+    <li 
+    v-for="item in {items}"
+    :key="item.title">
+      {{ item.title }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class NavigationList extends Vue {
+  @Prop() private items!: [string];
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
